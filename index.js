b85362a9b59e7ec4c384a931dd1bcfd0453800de
Shareif Hall <shareifhall@yahoo.com>
2023-02-11 10:54:27 -0500

    commit tree 4eebeef5010618438fd4b75e748a672d0312ffff

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee = {name :"George", 
+streetAdress : "1 Park ave, Kansas City PA"};
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const updateEmployee = {...obj};
+    updateEmployee[key] = value;
+    return updateEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+function deleteFromEmployeeByKey(obj, key) {
+    const newEmployee = {...obj};
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
+
